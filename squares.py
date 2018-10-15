75e1523d87dec1cca3a01e2b3b3684a3bcc8d1c4
AgnesBelt <31009972+AgnesBelt@users.noreply.github.com>
2018-10-15 15:07:55 +0200

    Introduce function

--- squares.py
+++ squares.py
@@ -10,6 +10,12 @@ print(2**2)
 print(3**2)
 print(4**2)
 
-
-for i in [1,2,3,4]:
-    print(i**2)
+def list_squares(n):
+    "return a list of sqared numbers"
+    lis=[]
+    for i in range(1, n+1):
+        lis.append(i**2)
+    return(lis)
+    
+for i in list_squares(4):
+    print(i)
