b3974b57baeeedd164a0b45a4e2c666cb52da659
AgnesBelt <31009972+AgnesBelt@users.noreply.github.com>
2018-10-15 15:21:27 +0200

    Generalize from squares to powers

--- squares.py
+++ squares.py
@@ -5,17 +5,12 @@ Created on Mon Oct 15 14:12:33 2018
 @author: beltramo
 """
 
-print(1**2)
-print(2**2)
-print(3**2)
-print(4**2)
-
-def list_squares(n):
-    "return a list of sqared numbers"
+def list_powers(n, m):
+    "return a list of powered numbers"
     lis=[]
     for i in range(1, n+1):
-        lis.append(i**2)
+        lis.append(i**m)
     return(lis)
     
-for i in list_squares(4):
+for i in list_powers(4, 3):
     print(i)
